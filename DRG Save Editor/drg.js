844154e928d63fe47e640b9811822a9d0322ac58
Dima <strongbaby1337@gmail.com>
2022-11-06 23:12:25 +0200

    Upd - drg season 3

--- DRG Save Editor/drg.js
+++ DRG Save Editor/drg.js
@@ -6,8 +6,9 @@ class DRG {
     const season_guids = [
       "A47D407EC0E4364892CE2E03DE7DF0B3",
       "B860B55F1D1BB54D8EE2E41FDA9F5838",
+      "D8810F6C76D374419AE6A18EF5B3BA26",
     ]
-    const currentSeasonIndex = 1
+    const currentSeasonIndex = 2
     this.season_guid = season_guids[currentSeasonIndex]
     this.season_xp_offset = 48
     this.scrip_offset = 88
