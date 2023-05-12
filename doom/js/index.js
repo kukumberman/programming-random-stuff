9d2e35e2b1c7470e90b6052ec5228a1a64ffc036
Dima <strongbaby1337@gmail.com>
2023-05-13 00:14:37 +0300

    Add - doom - nodes & sectors

--- doom/js/index.js
+++ doom/js/index.js
@@ -11,6 +11,8 @@ const LUMP_INDICES = {
   BLOCKMAP: 10,
 }
 
+const SUBSECTOR_IDENTIFIER = 0x8000 // 32768
+
 const DOOM_WIDTH = 320
 const DOOM_HEIGHT = 200
 
@@ -150,7 +152,35 @@ class PanAndZoom {
   }
 }
 
+class Player {
+  constructor(x, y, angle, fov) {
+    this.x = x
+    this.y = y
+    this.angle = angle
+    this.fov = fov
+  }
+
+  moveForward(amount) {
+    const angle = (this.angle - 90) * Mathf.DEG_2_RAD
+    this.x += Math.sin(angle) * amount
+    this.y += Math.cos(angle) * amount
+  }
+
+  moveRight(amount) {
+    const angle = this.angle * Mathf.DEG_2_RAD
+    this.x += Math.sin(angle) * amount
+    this.y += Math.cos(angle) * amount
+  }
+
+  rotate(amount) {
+    this.angle += amount
+  }
+}
+
 class Mathf {
+  static DEG_2_RAD = Math.PI / 180
+  static RAD_2_DEG = 180 / Math.PI
+
   static remap(value, x1, y1, x2, y2) {
     return ((value - x1) * (y2 - x2)) / (y1 - x1) + x2
   }
@@ -229,12 +259,59 @@ class WadReader {
     }
   }
 
+  readNode(offset) {
+    return {
+      splitter: {
+        x: this.readInt16(offset + 0),
+        y: this.readInt16(offset + 2),
+        dx: this.readInt16(offset + 4),
+        dy: this.readInt16(offset + 6),
+      },
+      rightBbox: {
+        top: this.readInt16(offset + 8),
+        bottom: this.readInt16(offset + 10),
+        left: this.readInt16(offset + 12),
+        right: this.readInt16(offset + 14),
+      },
+      leftBbox: {
+        top: this.readInt16(offset + 16),
+        bottom: this.readInt16(offset + 18),
+        left: this.readInt16(offset + 20),
+        right: this.readInt16(offset + 22),
+      },
+      rightChild: this.readUint16(offset + 24),
+      leftChild: this.readUint16(offset + 26),
+    }
+  }
+
+  readSubsector(offset) {
+    return {
+      segCount: this.readUint16(offset + 0),
+      firstSegment: this.readUint16(offset + 2),
+    }
+  }
+
+  readSegment(offset) {
+    return {
+      startVertex: this.readInt16(offset + 0),
+      endVertex: this.readInt16(offset + 2),
+      angle: this.readInt16(offset + 4),
+      linedef: this.readInt16(offset + 6),
+      direction: this.readInt16(offset + 8),
+      offset: this.readInt16(offset + 10),
+    }
+  }
+
   readInt32(offset) {
     return this.dataView.getInt32(offset, true)
   }
 
   readInt16(offset) {
     return this.dataView.getInt16(offset, true)
+  }
+
+  readUint16(offset) {
+    return this.dataView.getUint16(offset, true)
   }
 
   readString(offset, length) {
@@ -293,6 +370,22 @@ class WadRuntime {
       (offset) => wad.reader.readThing(offset),
       10
     )
+    this.nodes = wad.getLumpData(
+      this.mapIndex + LUMP_INDICES.NODES,
+      (offset) => wad.reader.readNode(offset),
+      28
+    )
+    this.rootNodeIndex = this.nodes.length - 1
+    this.subsectors = wad.getLumpData(
+      this.mapIndex + LUMP_INDICES.SSECTORS,
+      (offset) => wad.reader.readSubsector(offset),
+      4
+    )
+    this.segments = wad.getLumpData(
+      this.mapIndex + LUMP_INDICES.SEGS,
+      (offset) => wad.reader.readSegment(offset),
+      12
+    )
   }
 }
 
@@ -320,6 +413,27 @@ class Renderer {
     this.ctx.lineTo(x2, y2)
     this.ctx.closePath()
     this.ctx.stroke()
+  }
+
+  rect(x, y, w, h, color) {
+    this.ctx.strokeStyle = color
+    this.ctx.strokeRect(x, y, w, h)
+  }
+
+  rectLegacy(x, y, w, h, color) {
+    const lines = [
+      { x, y },
+      { x: x + w, y },
+      { x: x + w, y: y + h },
+      { x: x, y: y + h },
+    ]
+
+    for (let i = 0, length = lines.length; i < length; i++) {
+      const j = (i + 1) % length
+      const p1 = lines[i]
+      const p2 = lines[j]
+      this.line(p1.x, p1.y, p2.x, p2.y, color)
+    }
   }
 
   clear(color) {
@@ -376,6 +490,43 @@ class MapTopDownRenderer {
     }
   }
 
+  drawSplitter(splitter) {
+    const v1 = { x: splitter.x, y: splitter.y }
+    const v2 = { x: splitter.x + splitter.dx, y: splitter.y + splitter.dy }
+    const p1 = this.remapVertex(v1)
+    const p2 = this.remapVertex(v2)
+    this.renderer.line(p1.x, p1.y, p2.x, p2.y, "blue")
+  }
+
+  drawBbox(bbox, color) {
+    const p1 = this.remapVertex({ x: bbox.left, y: bbox.top })
+    const p2 = this.remapVertex({ x: bbox.right, y: bbox.bottom })
+    const width = p2.x - p1.x
+    const height = p2.y - p1.y
+    this.renderer.rect(p1.x, p1.y, width, height, color)
+  }
+
+  drawNode(index) {
+    const node = this.wad.nodes[index]
+    this.drawBbox(node.rightBbox, "green")
+    this.drawBbox(node.leftBbox, "red")
+    this.drawSplitter(node.splitter)
+  }
+
+  drawSubsector(index, color) {
+    const sector = this.wad.subsectors[index]
+    for (let i = 0, length = sector.segCount; i < length; i++) {
+      this.drawSegment(sector.firstSegment + i, color)
+    }
+  }
+
+  drawSegment(index, color) {
+    const segment = this.wad.segments[index]
+    const p1 = this.remapVertex(this.wad.vertexes[segment.startVertex])
+    const p2 = this.remapVertex(this.wad.vertexes[segment.endVertex])
+    this.renderer.line(p1.x, p1.y, p2.x, p2.y, color)
+  }
+
   drawLines() {
     for (let i = 0, length = this.wad.linedefs.length; i < length; i++) {
       const line = this.wad.linedefs[i]
@@ -410,6 +561,31 @@ class MapTopDownRenderer {
     this.renderer.circle(point.x, point.y, 2, color)
   }
 
+  drawPlayerAndFov(player, color) {
+    const radius = 500
+
+    const angle = player.angle - player.fov * 0.5
+    const leftAngle = (angle - 90) * Mathf.DEG_2_RAD
+    const rightAngle = angle * Mathf.DEG_2_RAD
+
+    const leftPoint = {
+      x: player.x + Math.sin(leftAngle) * radius,
+      y: player.y + Math.cos(leftAngle) * radius,
+    }
+    const rightPoint = {
+      x: player.x + Math.sin(rightAngle) * radius,
+      y: player.y + Math.cos(rightAngle) * radius,
+    }
+
+    const p = this.remapVertex(player)
+    const p1 = this.remapVertex(leftPoint)
+    const p2 = this.remapVertex(rightPoint)
+
+    this.renderer.circle(p.x, p.y, 5, color)
+    this.renderer.line(p.x, p.y, p1.x, p1.y, color)
+    this.renderer.line(p.x, p.y, p2.x, p2.y, color)
+  }
+
   remapVertex(vertex) {
     return {
       x: this.remapX(vertex.x),
@@ -433,6 +609,32 @@ class MapTopDownRenderer {
   }
 }
 
+class MapData {
+  constructor(wadRuntime) {
+    this.wad = wadRuntime
+    const thing = wadRuntime.things[0]
+    this.player = new Player(thing.x, thing.y, thing.angle, 90)
+  }
+
+  pointOnLeftSide(x, y, node) {
+    const dx = x - node.splitter.x
+    const dy = y - node.splitter.y
+    return dx * node.splitter.dy - dy * node.splitter.dx <= 0
+  }
+
+  //! R_PointInSubsector
+  pointInSubsector(x, y) {
+    let nodeIndex = this.wad.rootNodeIndex
+
+    while (!(nodeIndex & SUBSECTOR_IDENTIFIER)) {
+      const node = this.wad.nodes[nodeIndex]
+      nodeIndex = this.pointOnLeftSide(x, y, node) ? node.leftChild : node.rightChild
+    }
+
+    return nodeIndex & ~SUBSECTOR_IDENTIFIER
+  }
+}
+
 class App {
   constructor(canvas) {
     this.canvas = canvas
@@ -452,9 +654,10 @@ class App {
 
   create() {
     const wad = new WadData(this.bytes)
-    const wadRuntime = new WadRuntime("E1M1", wad)
+    this.wadRuntime = new WadRuntime("E1M1", wad)
     this.renderer = new Renderer(this.canvas)
-    this.mapTopDownRenderer = new MapTopDownRenderer(this.renderer, wadRuntime)
+    this.mapTopDownRenderer = new MapTopDownRenderer(this.renderer, this.wadRuntime)
+    this.mapData = new MapData(this.wadRuntime)
   }
 
   resize(width, height) {
@@ -468,6 +671,17 @@ class App {
     this.mapTopDownRenderer.drawLines()
     this.mapTopDownRenderer.drawVertexes()
     this.mapTopDownRenderer.drawDefaultPlayer()
+
+    // for (let i = 0; i < this.wadRuntime.segments.length; i++) {
+    //   this.mapTopDownRenderer.drawSegment(i, "blue")
+    // }
+
+    const player = this.mapData.player
+
+    const sectorIndex = this.mapData.pointInSubsector(player.x, player.y)
+    this.mapTopDownRenderer.drawSubsector(sectorIndex, "blue")
+
+    this.mapTopDownRenderer.drawPlayerAndFov(player, "red")
   }
 
   renderLoop(time) {
@@ -497,6 +711,50 @@ async function main() {
   await app.preload()
   app.create()
   app.startRenderLoop()
+
+  const player = app.mapData.player
+
+  const config = {
+    angle: 30,
+    speed: 150,
+  }
+
+  window.addEventListener("keydown", (event) => {
+    if (event.repeat) {
+      return
+    }
+
+    switch (event.code) {
+      case "KeyQ":
+        player.rotate(-config.angle)
+        break
+      case "KeyE":
+        player.rotate(config.angle)
+        break
+      case "KeyW":
+        player.moveForward(config.speed)
+        break
+      case "KeyS":
+        player.moveForward(-config.speed)
+        break
+      case "KeyA":
+        player.moveRight(-config.speed)
+        break
+      case "KeyD":
+        player.moveRight(config.speed)
+        break
+    }
+  })
+
+  canvas.addEventListener("mousemove", (event) => {
+    if (event.ctrlKey) {
+      const pos = app.panAndZoom.transform.transform({
+        x: event.clientX,
+        y: event.clientY,
+      })
+      //todo: calculate and set player position to follow cursor
+    }
+  })
 }
 
 main()
